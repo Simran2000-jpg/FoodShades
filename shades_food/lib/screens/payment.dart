fd5711924446c602afb0dc08b42192dcee86d38e
srijan-bytes <srijansaxena18@gmail.com>
2021-10-21 17:49:38 +0530

    added payment

---
+++ shades_food/lib/screens/payment.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class Payment extends StatefulWidget {
+  @override
+  _PaymentState createState() => _PaymentState();
+}
+
+class _PaymentState extends State<Payment> {
+  Razorpay razorpay = Razorpay();
+  TextEditingController textEditingController = new TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    razorpay = new Razorpay();
+    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
+    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerPaymentError);
+    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    razorpay.clear();
+  }
+
+  void openCheckout() {
+    var options = {
+      "key": "rzp_test_xmnLBhmX7WPCYC",
+      "amount": num.parse(textEditingController.text) * 100,
+      "name": "Sample App",
+      "description": "Payment for the some random product",
+      "prefill": {
+        "contact": "8081360868",
+        "email": "srijansaxena679@gmail.com"
+      },
+      "external": {
+        "wallets": ["paytm"]
+      }
+    };
+
+    try {
+      razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void handlerPaymentSuccess() {
+    print("Payment Success");
+  }
+
+  void handlerPaymentError() {
+    print("Payment Error");
+  }
+
+  void handlerExternalWallet() {
+    print("External Wallet");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Razor Pay"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Column(children: <Widget>[
+          TextField(
+            controller: textEditingController,
+            decoration: InputDecoration(
+              hintText: "Enter Amount",
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                openCheckout();
+              },
+              child: Text("PAY")),
+        ]),
+      ),
+    );
+  }
+}
